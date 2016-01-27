d41d6301496df256ed78ee004153f5f09059ee34
destyjustc <kjoshy12@gmail.com>
2016-01-26 23:52:48 -0800

    update template, add form, update backend

--- firststeps.py
+++ firststeps.py
@@ -1,7 +1,7 @@
-from sqlalchemy.orm import sessionmaker
-import time
+import time, datetime
 import json
 import os
+import random
 from pprint import pprint
 from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_pyfile('app.cfg')
 db = SQLAlchemy(app)
+current_week = "1"
 
 class User(db.Model):
 	__tablename__ = 'users'
@@ -46,10 +47,12 @@ class Play(db.Model):
 
 	id = db.Column(db.Integer, primary_key=True)
 	date = db.Column(db.String)
-	players = db.Column(db.String)
+	user_id = db.Column(db.Integer)
+	week = db.Column(db.String)
+	luck = db.Column(db.Float)
 
 	def __repr__(self):
-		return "<Play(date='%s', player='%s')>" % (self.date, self.players)
+		return "<Play(date='%s', user_id='%d', luck='%f')>" % (self.date, self.user_id, self.luck)
 
 
 def initJackPot():
@@ -70,25 +73,55 @@ def initUsers():
 		db.session.commit()
 
 def initPlay():
-	play = Play(date=time.strftime("%x"), players="")
+	play = Play(date=time.strftime("%x"))
 	db.session.add(play)
 	db.session.commit()
 
 @app.route('/', methods=["GET", "POST"])
 def home_page():
 	if request.method == "POST":
-		print User.query.order_by(User.name.asc()).all()
-	return render_template("home.html", users=User.query.order_by(User.name.asc()).all(), jackpots=JackPot.query.all(), play=Play.query.all())
+		if request.form['id']:
+			tmp = request.form['id']
+			play = Play.query.filter_by(week=current_week, user_id=tmp).first()
+			if not play:
+				play = Play(date=datetime.datetime.utcnow(), user_id=int(tmp), week=current_week, luck=random.randrange(-100, 100))
+				print play
+				db.session.add(play)
+				db.session.commit()
+			# print "hello"
+			# print tmp
+		# if request.form['name']:
+		# 	tmp_name = request.form['name']
+		# 	usr = User.query.filter_by(name=tmp_name).first()
+		# 	play = Play.query.filter_by(week=current_week, user_id=usr.id).first()
+		# 	print 'hello'
+		# 	print play
+		# 	print usr
+		# 	if not play:
+		# 		play = Play(date=datetime.datetime.utcnow(), user_id=usr.id, week=current_week)
+		# 		db.session.add(play)
+		# 		db.session.commit()
+		# print Play.query.all()
+
+		return redirect(url_for('home_page'))
+	# play_list = User.query.join(Play, User.id = Play.user_id).filter(Play.week = current_week).add_columns(Users.name, Play.luck).order_by(Play.luck.desc())
+	play_list = User.query.join(Play, (User.id == Play.user_id)).filter(Play.week==current_week).add_columns(User.name, Play.luck).order_by(Play.luck.desc()).all()
+	return render_template("home.html", users=User.query.order_by(User.name.asc()).all(), jackpots=JackPot.query.all(), play=Play.query.order_by(Play.luck).all(), play_list=play_list)
 
 	Comments.query.order_by(Comments.pub_date.desc()).all()
 
 # print time.strftime("%x")
 
 if __name__ == '__main__':
-	db.create_all()
+	# db.drop_all()
+	# db.create_all()
+	# User.query.delete()
+	# JackPot.query.delete()
+	# Play.query.delete()
 	# initUsers()
 	# initJackPot()
-	# initPlay()
+	# print User.query.all()
+	# print JackPot.query.all()
 	app.run( 
         host="0.0.0.0",
         port=int("6490")
